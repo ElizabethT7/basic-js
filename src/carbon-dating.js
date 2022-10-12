73f06dfb7fa966a7dbc7135438f9cdf044c36133
Liza Timofeeva <tymofieievai@gmail.com>
2022-10-12 22:00:26 +0300

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,31 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (typeof sampleActivity !== "string" || sampleActivity === null || sampleActivity === undefined ||
+   sampleActivity.length <= 0 || 
+  sampleActivity === NaN || sampleActivity.match(/[^\d\.EXABCDEF\-\+]/gi))  return false; //if(object.match(/[^\d\.EXABCDEF\-\+]/gi) || isNaN(parseFloat(object))) return false;
+  const radioactiveDecayConstant = Math.log(2) / HALF_LIFE_PERIOD;
+  console.log(radioactiveDecayConstant);
+  let number = Number(sampleActivity);
+  console.log(Number(sampleActivity))
+ // console.log(typeof number)
+  if (number > 15 || number <= 0 || number === Infinity) {return false;}
+  else {
+  let time = (Math.log(MODERN_ACTIVITY / Number(sampleActivity))) / radioactiveDecayConstant;
+  console.log(time);
+  return Math.ceil(time);}
 }
+
+//dateSample('9.59383373526808')//  3695
+
+dateSample('9000')//, false);
+dateSample('15.1')//, false);
+      dateSample('0')//, false);
+      dateSample('-5')//, false);
+      dateSample('-55.8')//, false);
 
 module.exports = {
   dateSample
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  console.log(members)
+  console.log(members.length)
+  let result = '';
+  if (members === null || members === undefined || members.length === 0) { return false;}
+  for (let i = 0; i < members.length; i++) {
+    console.log(typeof members[i]);
+    if (typeof (members[i]) == "string" || members[i] instanceof String) {
+      let name = members[i].trim().toUpperCase();
+      console.log(name);
+      //for (let j = 0; j< members[i].length; j++ ){
+      result = result + name[0];
+      console.log(name[0]);
+      console.log(result)
+    //}
+    } else { 
+      result = result;
+      i++;
+    } //return result; 
+    console.log(result)
+  }
+  
+  let dreamTeamName = result.split('').sort().join('');
+  console.log(dreamTeamName);
+  return dreamTeamName;
+
 }
+
+
+//createDreamTeam(['Dylan', 'Keith', 'Madison', 'Ralph', 'Olivia', 'Raymond', 'Megan', 'Frank', 'Daniel', 'Carol'])
+/*createDreamTeam([
+  '   William Alston ',
+  ' Paul Benacerraf',
+  '  Ross Cameron',
+  '       Gilles Deleuze',
+  '  Arda Denkel ',
+  '  Michael Devitt',
+  '  Kit Fine',
+  ' Nelson Goodman',
+  'David Kolb',
+  '   Saul Kripke',
+  '  Trenton Merricks',
+  '  Jay Rosenberg',
+])*/
+
+/*createDreamTeam([
+  'amelia',
+  'ruby',
+  'lily',
+  'grace',
+  'millie',
+  'daisy',
+  'freya',
+  'erin',
+  'megan',
+  'jasmine',
+  'brooke',
+])*/
+
+createDreamTeam([
+  ['David Abram'],
+  ['Robin Attfield'],
+  'Thomas Berry',
+  ['Paul R.Ehrlich'],
+  'donna Haraway',
+  ' BrIaN_gOodWiN  ',
+  {
+    0: 'Serenella Iovino'
+  },
+  'Erazim Kohak',
+  '  val_plumwood',
+])
 
 module.exports = {
   createDreamTeam
