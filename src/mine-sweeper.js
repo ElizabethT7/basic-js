1cf6a9a570b50ae6d5be18ba8d776f8eb6726037
Liza Timofeeva <tymofieievai@gmail.com>
2022-10-16 20:29:56 +0300

    feat:add mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,72 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+ const board = [];
+ for (let i = 0; i < matrix.length; i++) {
+  board.push([]);
+  for (let j = 0; j < matrix[0].length; j++) {
+   board[i][j] = 0;
+   //above
+   if(matrix[i - 1] !== undefined) {
+    if (matrix[i - 1][j]) { //true
+      board[i][j]++;
+    }
+   }
+   //below
+   if(matrix[i + 1] !== undefined) {
+    if (matrix[i + 1][j]) { //true
+      board[i][j]++;
+    }
+   }
+    //left
+    if(matrix[i][j - 1] !== undefined) {
+      if (matrix[i][j - 1]) { //true
+        board[i][j]++;
+      }
+    }
+    //right
+    if(matrix[i][j + 1] !== undefined) {
+      if (matrix[i][j + 1]) { //true
+        board[i][j]++;
+      }
+    }
+    //top left 
+    if(matrix[i - 1] !== undefined) {
+      if (matrix[i - 1][j - 1]) { //true
+        board[i][j]++;
+      }
+    }
+    //top right 
+    if(matrix[i - 1] !== undefined) {
+      if (matrix[i - 1][j + 1]) { //true
+        board[i][j]++;
+      }
+    }
+    //bottom left 
+    if(matrix[i + 1] !== undefined) {
+      if (matrix[i + 1][j - 1]) { //true
+        board[i][j]++;
+      }
+    }
+    //bottom right 
+    if(matrix[i + 1] !== undefined) {
+      if (matrix[i + 1][j + 1]) { //true
+        board[i][j]++;
+      }
+    }
+  }
+ }
+ console.log(board)
+
+ return board;
 }
+
+minesweeper([
+  [true, false, false],
+  [false, true, false],
+  [false, false, false],
+])
 
 module.exports = {
   minesweeper
--- src/what-season.js
+++ src/what-season.js
@@ -13,13 +13,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
  //let date = new Date(2150, 7, 21, 18, 36, 41, 841);
 function getSeason(date) {
- // throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
   if (date !== undefined) {
     let month = date.getMonth();
-   // if (Date.prototype.toString.call(new Date()) instanceof Date) throw new Error('Invalid date!');
+    if (Date.prototype.toString.call(new Date()) instanceof Date) {throw new Error('Invalid date!');}
     if (!(date instanceof Date) || Object.getOwnPropertyNames(date).length) {throw new Error('Invalid date!');}
-      //if (month > 12) throw new Error('Invalid date!');
+      if (month > 12 || month < 0) {throw new Error('Invalid date!');}
       if ((month >= 0 && month < 2) || month === 11) return 'winter';
       if ((month >= 2 && month < 5)) return 'spring';
       if ((month >= 5 && month < 8)) return 'summer';
